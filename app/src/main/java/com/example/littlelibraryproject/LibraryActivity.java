e4d4fa04906c3c31f96324c7b0e7923e742a0017
Corbot5000 <corbet@umich.edu>
2019-11-19 09:17:03 -0500

    Moved Add Photo Button to Library Page

--- app/src/main/java/com/example/littlelibraryproject/LibraryActivity.java
+++ app/src/main/java/com/example/littlelibraryproject/LibraryActivity.java
@@ -2,13 +2,34 @@ package com.example.littlelibraryproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LibraryActivity extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_library);
+
+        button = (Button) findViewById(R.id.button5);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                openAddPhotoActivity();
+            }
+        });
     }
+
+
+    public void openAddPhotoActivity() {
+        Intent intent = new Intent(this, AddPhotoActivity.class);
+        startActivity(intent);
+    }
+
+
 }
