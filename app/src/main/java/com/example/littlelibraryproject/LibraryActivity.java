5788dd1690a7997dc12063c436f5e1c09c7a64e4
terryz <terryz@umich.edu>
2019-11-19 09:34:25 -0500

    Register Account UI Rename

--- app/src/main/java/com/example/littlelibraryproject/LibraryActivity.java
+++ app/src/main/java/com/example/littlelibraryproject/LibraryActivity.java
@@ -16,7 +16,7 @@ public class LibraryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_library);
 
-        button = (Button) findViewById(R.id.button5);
+        button = (Button) findViewById(R.id.buttonRegisterStart);
         button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
