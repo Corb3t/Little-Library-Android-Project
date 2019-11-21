c76c80b9560adab0f9dd4be5efb053b458931e96
phildave55 <57492001+phildave55@users.noreply.github.com>
2019-11-21 09:50:05 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/example/littlelibraryproject/LibraryActivity.java
    #	app/src/main/res/layout/activity_library.xml

--- app/src/main/java/com/example/littlelibraryproject/LibraryActivity.java
+++ app/src/main/java/com/example/littlelibraryproject/LibraryActivity.java
@@ -1,35 +1,19 @@
 package com.example.littlelibraryproject;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class LibraryActivity extends AppCompatActivity {
 
-    private Button button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LibraryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_library);
-
-        button = (Button) findViewById(R.id.buttonRegisterStartPrompt);
-        button.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                openAddPhotoActivity();
-            }
-        });
     }
-
-
-    public void openAddPhotoActivity() {
-        Intent intent = new Intent(this, AddPhotoActivity.class);
-        startActivity(intent);
-    }
-
 
 }
