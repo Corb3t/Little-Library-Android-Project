b1c7fe3c2b4fc75d1b29ffb78acfccfcae4ea13b
joeblackhurst <josephblackhurst@gmail.com>
2019-11-21 09:30:39 -0500

    test

--- app/src/main/java/com/example/littlelibraryproject/MainActivity.java
+++ app/src/main/java/com/example/littlelibraryproject/MainActivity.java
@@ -2,18 +2,16 @@ package com.example.littlelibraryproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.example.littlelibraryproject.R;
-
 public class MainActivity extends AppCompatActivity {
     private Button button;
 
-    //variables  
+
+    //variables
     private boolean mLocationPermissionGranted = false;
 
     @Override
@@ -21,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = (Button) findViewById(R.id.button);
+        button = (Button) findViewById(R.id.buttonLoginRegister);
         button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
