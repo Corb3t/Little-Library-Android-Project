d38245ca33e88c784d061d16d223cabf6364dcc4
terryz <terryz@umich.edu>
2019-11-21 09:40:24 -0500

    Added navigation

--- app/src/main/java/com/example/littlelibraryproject/MainActivity.java
+++ app/src/main/java/com/example/littlelibraryproject/MainActivity.java
@@ -5,8 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.littlelibraryproject.R;
 
@@ -14,7 +18,6 @@ import com.example.littlelibraryproject.R;
 public class MainActivity extends AppCompatActivity {
     private Button button;
 
-    //variables  
     private boolean mLocationPermissionGranted = false;
 
     @Override
@@ -45,6 +48,35 @@ public class MainActivity extends AppCompatActivity {
                 openLibraryActivity();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.mainmenu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.itemLogin) {
+            Intent LoginIntent = new Intent(this, LoginActivity.class);
+            startActivity(LoginIntent);
+
+        } else if (item.getItemId() == R.id.itemMap) {
+            Intent MapIntent = new Intent(this, MapsActivity.class);
+            startActivity(MapIntent);
+        } else if (item.getItemId() == R.id.itemUsers) {
+            Intent UsersIntent = new Intent(this, UsersActivity.class);
+            startActivity(UsersIntent);
+        } else if (item.getItemId() == R.id.itemLibrary) {
+            Intent LibraryIntent = new Intent(this, LibraryActivity.class);
+            startActivity(LibraryIntent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void openMapsActivity() {
