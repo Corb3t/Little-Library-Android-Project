13fcb0723008539336bb7583affb7fb499eec847
joeblackhurst <josephblackhurst@gmail.com>
2019-11-26 12:10:26 -0500

    new comment

---
+++ app/src/main/java/com/example/littlelibraryproject/BadgeFivePhotosActivity.java
@@ -0,0 +1,14 @@
+package com.example.littlelibraryproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BadgeFivePhotosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_badge_five_photos);
+    }
+}
