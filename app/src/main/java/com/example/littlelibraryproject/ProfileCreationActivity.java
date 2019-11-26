54f105c4c4d0aec75d2df718b1a52e05a7376958
Corbot5000 <corbet@umich.edu>
2019-11-26 09:22:24 -0500

    added profile creation activity

--- app/src/main/java/com/example/littlelibraryproject/ProfileCreationActivity.java
+++ app/src/main/java/com/example/littlelibraryproject/ProfileCreationActivity.java
@@ -5,7 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class ProfileCreationActivity extends AppCompatActivity {
-
+    //test
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
